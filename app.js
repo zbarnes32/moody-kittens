922cdc43c1c148086b5254ee9266a4e0fe302a44
Jake Overall <joverall22@gmail.com>
2022-01-21 13:37:38 -0700

    student handoff

---
+++ app.js
@@ -0,0 +1,100 @@
+let kittens = []
+/**
+ * Called when submitting the new Kitten Form
+ * This method will pull data from the form
+ * use the provided function to give the data an id
+ * then add that data to the kittens list.
+ * Then reset the form
+ */
+function addKitten(event) {
+}
+
+/**
+ * Converts the kittens array to a JSON string then
+ * Saves the string to localstorage at the key kittens 
+ */
+function saveKittens() {
+}
+
+/**
+ * Attempts to retrieve the kittens string from localstorage
+ * then parses the JSON string into an array. Finally sets
+ * the kittens array to the retrieved array
+ */
+function loadKittens() {
+}
+
+/**
+ * Draw all of the kittens to the kittens element
+ */
+function drawKittens() {
+}
+
+
+/**
+ * Find the kitten in the array by its id
+ * @param {string} id 
+ * @return {Kitten}
+ */
+function findKittenById(id) {
+}
+
+
+/**
+ * Find the kitten in the array of kittens
+ * Generate a random Number
+ * if the number is greater than .5 
+ * increase the kittens affection
+ * otherwise decrease the affection
+ * @param {string} id 
+ */
+function pet(id) {
+}
+
+/**
+ * Find the kitten in the array of kittens
+ * Set the kitten's mood to tolerant
+ * Set the kitten's affection to 5
+ * @param {string} id
+ */
+function catnip(id) {
+}
+
+/**
+ * Sets the kittens mood based on its affection
+ * @param {Kitten} kitten 
+ */
+function setKittenMood(kitten) {
+}
+
+/**
+ * Removes all of the kittens from the array
+ * remember to save this change
+ */
+function clearKittens(){
+}
+
+function getStarted() {
+  document.getElementById("welcome").remove();
+  console.log('Good Luck, Take it away')
+}
+
+
+
+
+/**
+ * Defines the Properties of a Kitten
+ * @typedef {{name: string, mood: string, affection: number}} Kitten
+ */
+
+
+/**
+ * Used to generate a random string id for mocked
+ * database generated Id
+ * @returns {string}
+ */
+function generateId() {
+  return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
+}
+
+loadKittens();
